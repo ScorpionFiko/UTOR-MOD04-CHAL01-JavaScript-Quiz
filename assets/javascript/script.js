96ad73fe01f4bd2a9cf7e6a13c4c5db9ae09eaf2
Stefan Marinov <stefan_marinov@rogers.com>
2023-01-22 11:56:45 -0500

    implemented timer display

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,8 @@ import { loadQuestions, loadUserScore } from "./data.js";
 let startBtn = document.getElementById("startQuiz");
 let quizQuestions = loadQuestions();
 let userScores = [];
+let quizTimer = 120;
+let penalty = 10;
 /* testing code below */
 quizQuestions.forEach((quizQuestion, qIndex) => {
     console.log("Q" + (qIndex + 1) + ". " + quizQuestion.quesetion);
@@ -14,8 +16,8 @@ quizQuestions.forEach((quizQuestion, qIndex) => {
 /* testing code above */
 
 init();
-
 function init() {
+
     // initializing by setting up the questions
     // and getting any stored user scores
     quizQuestions = loadQuestions();
@@ -52,4 +54,33 @@ function displayQuestion() {
 
 function startTimer() {
     console.log("startTimer");
+    var quizInterval = setInterval(function () {
+        // reducing our quizTimer
+        quizTimer--;
+        // reducing the timer by any penalties for not answering correctly
+        quizTimer -= penalty;
+        // if quizTimer > 0 then display time
+        // otherwise stop the timer and display the finale page
+        displayQuizTimer(quizTimer);
+        if (quizTimer > 0) {
+            //resetting the penalty
+            //penalty=0;
+        }
+        else {
+            clearInterval(quizInterval);
+            // display final page
+        }
+    }, 1000);
+}
+
+function displayQuizTimer(quizTimer) {
+    if (quizTimer < 0) {
+        quizTimer = 0;
+    }
+    let seconds = parseInt(quizTimer % 60);
+    let minutes = parseInt(quizTimer / 60);
+    document.getElementById("timer").innerHTML = 
+        "Time left: " + 
+        ((minutes >= 10 ) ? minutes : ("0" + minutes)) + ":" +
+        ((seconds >= 10 ) ? seconds : ("0" + seconds));
 }
